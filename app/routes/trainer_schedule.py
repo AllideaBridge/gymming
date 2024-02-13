2bf3a39e6e510911b09c7d841e86c652c2bffcd3
TaeYeong Im <imtaebari@gmail.com>
2024-02-13 22:45:24 +0900

    트레이너 하루 스케쥴 조회 api & test code

--- app/routes/trainer_schedule.py
+++ app/routes/trainer_schedule.py
@@ -1,6 +1,7 @@
 from calendar import monthrange
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
+from flask import jsonify
 from flask_restx import Namespace, Resource, fields
 from sqlalchemy import extract, func
 
@@ -15,7 +16,7 @@ ns_trainer_schedule = Namespace('trainer_schedule', description='Trainer Schedul
 
 # 입력 받은 year, month중 트레이너의 근무 스케쥴이 꽉 차지 않은 날짜 배열을 리턴한다.
 @ns_trainer_schedule.route('/<int:trainer_id>/<int:year>/<int:month>')
-class TrainerSchedule(Resource):
+class TrainerMonthSchedule(Resource):
     def get(self, trainer_id, year, month):
         # 1단계: 트레이너의 전체 가능 요일 조회
         available_week_days = db.session.query(
@@ -61,3 +62,53 @@ class TrainerSchedule(Resource):
         # "근무 가능 날짜" 목록에서 조건을 충족하는 날짜를 제외한 결과 반환
         return sorted(list(available_dates))
 
+
+@ns_trainer_schedule.route('/<int:trainer_id>/<int:year>/<int:month>/<int:day>')
+class TrainerDaySchedule(Resource):
+    def get(self, trainer_id, year, month, day):
+        date_filter = date(year, month, day)
+
+        trainer_details = db.session.query(
+            Trainer.lesson_minutes,
+            TrainerAvailability.start_time,
+            TrainerAvailability.end_time,
+        ) \
+            .join(TrainerAvailability, (TrainerAvailability.trainer_id == Trainer.trainer_id) \
+                  & (TrainerAvailability.week_day == date_filter.weekday())) \
+            .filter(Trainer.trainer_id == trainer_id,
+                    Trainer.trainer_delete_flag == False) \
+            .first()  # 가정: 모든 스케줄에 대해 lesson_minutes와 availability_start_time는 동일
+
+        if trainer_details:
+            formatted_start_time = trainer_details.start_time.strftime(
+                "%H:%M") if trainer_details.start_time else None
+            formatted_end_time = trainer_details.end_time.strftime(
+                "%H:%M") if trainer_details.end_time else None
+
+            result = {
+                'lesson_minutes': trainer_details.lesson_minutes,
+                'availability_start_time': formatted_start_time,
+                'availability_end_time': formatted_end_time
+            }
+        else:
+            result = {}
+
+        schedules = db.session.query(
+            Schedule.schedule_id,
+            Schedule.schedule_start_time
+        ) \
+            .join(TrainingUser, (TrainingUser.training_user_id == Schedule.training_user_id) \
+                  & (TrainingUser.training_user_delete_flag == False) \
+                  & (Schedule.schedule_delete_flag == False) \
+                  & (db.func.date(Schedule.schedule_start_time) == date_filter)) \
+            .join(Trainer, (Trainer.trainer_id == TrainingUser.trainer_id) \
+                  & (Trainer.trainer_id == trainer_id) \
+                  & (Trainer.trainer_delete_flag == False)) \
+            .all()
+
+        result['schedules'] = [{
+            'schedule_id': schedule.schedule_id,
+            'schedule_start_time': schedule.schedule_start_time
+        } for schedule in schedules]
+
+        return jsonify(result)
--- tests/test_trainer_schedule_api.py
+++ tests/test_trainer_schedule_api.py
@@ -34,7 +34,7 @@ class TrainerScheduleTestCase(unittest.TestCase):
             training_user = TrainingUser(trainer_id=trainer.trainer_id, user_id=user.user_id)
             db.session.add(training_user)
             db.session.commit()
-            start_time = datetime(2024, 1, 7 + i)
+            start_time = datetime(2024, 1, 7 + i, 10 + i, 30)
             for j in range(3):
                 schedule = Schedule(training_user_id=training_user.training_user_id,
                                     schedule_start_time=start_time + timedelta(days=j))
@@ -44,8 +44,8 @@ class TrainerScheduleTestCase(unittest.TestCase):
         # TrainerAvailability 레코드 추가
         for i in range(3):
             trainer_availability = TrainerAvailability(trainer_id=trainer.trainer_id, week_day=i + 1,
-                                                       start_time=datetime.now().time(),
-                                                       end_time=(datetime.now() + timedelta(hours=1)).time(),
+                                                       start_time=(start_time + timedelta(hours=i)).time(),
+                                                       end_time=(start_time + timedelta(hours=9)).time(),
                                                        possible_lesson_cnt=2 + i)
             db.session.add(trainer_availability)
 
@@ -68,3 +68,12 @@ class TrainerScheduleTestCase(unittest.TestCase):
         response = self.client.get(f'/trainer_schedule/{no_trainer_availability_trainer_id}/2024/1')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get_json(), [])
+
+    def test_트레이너_하루_스케쥴(self):
+        trainer_id = 1
+        response = self.client.get(f'/trainer_schedule/{trainer_id}/2024/01/10')
+        self.assertEqual(response.get_json(),
+                         {'availability_end_time': '22:30', 'availability_start_time': '14:30', 'lesson_minutes': 60,
+                          'schedules': [{'schedule_id': 3, 'schedule_start_time': 'Wed, 10 Jan 2024 11:30:00 GMT'},
+                                        {'schedule_id': 5, 'schedule_start_time': 'Wed, 10 Jan 2024 12:30:00 GMT'},
+                                        {'schedule_id': 7, 'schedule_start_time': 'Wed, 10 Jan 2024 13:30:00 GMT'}]})
