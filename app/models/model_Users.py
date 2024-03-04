bfe504538627c897e5d0b53bb338980874684c7f
TaeYeong Im <imtaebari@gmail.com>
2024-03-04 21:02:26 +0900

    User모델 이름, 번호 컬럼 필수 지정, 생년월일 컬럼 추가.

--- app/models/model_Users.py
+++ app/models/model_Users.py
@@ -4,9 +4,10 @@ from database import db
 class Users(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     user_email = db.Column(db.String(100), nullable=False)
-    user_name = db.Column(db.String(20), nullable=True)
+    user_name = db.Column(db.String(20), nullable=False)
     user_gender = db.Column(db.String(5), nullable=True)
-    user_phone_number = db.Column(db.String(20), nullable=True)
+    user_phone_number = db.Column(db.String(20), nullable=False)
     user_profile_img_url = db.Column(db.String(255), nullable=True)
     user_delete_flag = db.Column(db.Boolean, default=False)
     user_login_platform = db.Column(db.String(20), nullable=False)
+    user_birthday = db.Column(db.String(20), nullable=True)
