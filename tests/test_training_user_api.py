ce76449fc55d337d546b0aaecaad103be657ca75
TaeYeong Im <imtaebari@gmail.com>
2024-03-14 20:33:07 +0900

    트레이닝 회원 정보 수정, 삭제 api 테스트 코드

--- tests/test_training_user_api.py
+++ tests/test_training_user_api.py
@@ -1,3 +1,4 @@
+import http
 import unittest
 from datetime import datetime, timedelta
 
@@ -49,7 +50,7 @@ class TrainerUserTestCase(unittest.TestCase):
                 'special_notes': '무릎 부상 주의'
             }
 
-            response = self.client.post('/training-user/register', json=data)
+            response = self.client.post('/training-user/user', json=data)
             self.assertEqual(response.status_code, 200)
             self.assertIn('새로운 회원이 등록되었습니다.', response.json['message'])
 
@@ -118,3 +119,50 @@ class TrainerUserTestCase(unittest.TestCase):
         self.assertEqual(user.user_delete_flag, data['user_delete_flag'])
         self.assertEqual(user.user_login_platform, data['user_login_platform'])
         self.assertEqual(user.user_birthday, data['user_birthday'])
+
+    def test_트레이닝_회원_수정(self):
+        # Trainer 레코드 추가
+        trainer = Trainer(trainer_name='Test Trainer', trainer_email='test@example.com',
+                          trainer_gender='M', trainer_phone_number='010-0000-0000', lesson_minutes=60,
+                          lesson_change_range=3)
+        db.session.add(trainer)
+        db.session.commit()
+
+        # Users 데이터 생성
+        user = Users(
+            user_email="test@example.com",
+            user_name="Test User",
+            user_gender="M",
+            user_phone_number="010-1234-5678",
+            user_profile_img_url="http://example.com/profile.jpg",
+            user_login_platform="test_platform"
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        # TrainingUser 데이터 생성
+        training_user = TrainingUser(trainer_id=trainer.trainer_id, user_id=user.user_id)
+        db.session.add(training_user)
+        db.session.commit()
+
+        training_user_id = training_user.training_user_id
+        lesson_total_count = 10
+        lesson_current_count = 10
+        response = self.client.put('/training-user/user', json={
+            'training_user_id': training_user_id,
+            'lesson_total_count': lesson_total_count,
+            'lesson_current_count': lesson_current_count
+        })
+
+        self.assertEqual(response.status_code, http.HTTPStatus.OK)
+
+        training_user_from_db = TrainingUser.query.filter_by(training_user_id=training_user_id).first()
+        self.assertEqual(training_user_from_db.lesson_current_count, lesson_current_count)
+        self.assertEqual(training_user_from_db.lesson_total_count, lesson_total_count)
+
+        self.client.delete('/training-user/user', json={
+            'training_user_id': training_user_id
+        })
+
+        training_user_from_db = TrainingUser.query.filter_by(training_user_id=training_user_id).first()
+        self.assertTrue(training_user_from_db.training_user_delete_flag)
