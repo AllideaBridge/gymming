556363e817cdb4a81bd31f74b56c6823d4150c79
TaeYeong Im <imtaebari@gmail.com>
2024-03-25 17:59:17 +0900

    스케쥴 변경시 조건 추가, 문서에 파라미터 정보 추가.

--- app/routes/route_schedule.py
+++ app/routes/route_schedule.py
@@ -2,7 +2,6 @@ import logging
 from datetime import datetime, date
 from calendar import monthrange
 
-from flask import jsonify
 from flask_restx import Namespace, Resource, fields
 from sqlalchemy import func
 from sqlalchemy.sql.functions import coalesce
@@ -32,6 +31,12 @@ user_schedule_model = ns_schedule.model('UserSchedule', {
 # 회원의 한달 중 스케쥴이 있는 날짜 조회.
 @ns_schedule.route('/<int:user_id>/<int:year>/<int:month>')
 class UserMonthScheduleList(Resource):
+    @ns_schedule.doc(description='회원의 한달 중 스케쥴이 있는 날짜를 조회합니다.',
+                     params={
+                         'user_id': '유저 id',
+                         'year': '년도',
+                         'month': '월'
+                     })
     def get(self, user_id, year, month):
         schedules = db.session.query(func.distinct(func.date(Schedule.schedule_start_time))).join(TrainingUser).filter(
             TrainingUser.user_id == user_id,
@@ -40,12 +45,19 @@ class UserMonthScheduleList(Resource):
 
         scheduled_dates = [schedule[0].strftime(DATEFORMAT) for schedule in schedules]
 
-        return sorted(scheduled_dates)
+        return {'dates': sorted(scheduled_dates)}
 
 
 @ns_schedule.route('/<int:user_id>/<int:year>/<int:month>/<int:day>')
 class UserDayScheduleList(Resource):
     @ns_schedule.marshal_with(user_schedule_model)
+    @ns_schedule.doc(description='회원의 해당 날의 스케쥴 리스트 조회.',
+                     params={
+                         'user_id': '유저 id',
+                         'year': '년도',
+                         'month': '월',
+                         'day': '일'
+                     })
     def get(self, user_id, year, month, day):
         schedules = db.session.query(
             Trainer.trainer_name,
@@ -68,35 +80,46 @@ class UserDayScheduleList(Resource):
 
 # todo: 스케쥴 조회시 스케쥴 상태 조건 추가
 
-# todo: 스케쥴 변경 시 새로운 스케쥴이 중복되지 않는지 어떻게 확인 후 처리할까? -> 스케쥴을 먼저 추가한 후 삭제 진행.
 @ns_schedule.route('/<int:schedule_id>/change')
 class ScheduleChangeResource(Resource):
+    @ns_schedule.doc(description='스케쥴 변경 가능 기간인 경우 스케쥴 변경 api',
+                     params={
+                         'schedule_id': '스케쥴 id',
+                         'request_time': '2024-01-10 12:30:00(%Y-%m-%d %H:%M:%S)'
+                     })
     def post(self, schedule_id):
         body = ns_schedule.payload
-        requested_date = body['requested_date']
+        request_time_str = body['request_time']
 
         try:
-            requested_date = datetime.strptime(requested_date, DATETIMEFORMAT)
+            request_time = datetime.strptime(request_time_str, DATETIMEFORMAT)
         except ValueError as e:
             logging.log(logging.ERROR, e)
-            return jsonify({'error': 'Invalid date format'}), 400
+            return {'error': 'Invalid date format'}, 400
 
         try:
             # 트랜잭션 시작
             with db.session.begin_nested():
                 # 스케줄 상태 업데이트
-                schedule = Schedule.query.filter_by(schedule_id=schedule_id).first()
+                schedule = Schedule.find_by_id(schedule_id)
                 if not schedule:
                     return {'error': 'Schedule not found'}, 404
+
+                training_user = TrainingUser.find_by_id(schedule.training_user_id)
+                trainer_id = training_user.trainer_id
+
+                conflict_schedule = Trainer.conflict_trainer_schedule(trainer_id, request_time)
+
+                if conflict_schedule:
+                    # 충돌하는 스케줄이 있는 경우
+                    return {'message': 'New schedule conflicts with existing schedules of the trainer'}, 400
 
                 schedule.schedule_status = SCHEDULE_CANCELLED
                 db.session.add(schedule)
 
-                # 새 스케줄 생성
-                # todo : 새 스케쥴 생성시 시간 중복 여부 체크
                 new_schedule = Schedule(
                     training_user_id=schedule.training_user_id,
-                    schedule_start_time=requested_date,
+                    schedule_start_time=request_time,
                     schedule_status=SCHEDULE_SCHEDULED  # 새 스케줄의 상태를 'scheduled'로 설정
                 )
                 db.session.add(new_schedule)
@@ -113,6 +136,10 @@ class ScheduleChangeResource(Resource):
 
 @ns_schedule.route('/<int:schedule_id>/cancel')
 class ScheduleCancelResource(Resource):
+    @ns_schedule.doc(description='스케쥴 변경 가능 기간인 경우 스케쥴 취소 api',
+                     params={
+                         'schedule_id': '스케쥴 id',
+                     })
     def post(self, schedule_id):
         schedule = Schedule.query.filter_by(schedule_id=schedule_id).first()
         if not schedule:
@@ -127,6 +154,12 @@ class ScheduleCancelResource(Resource):
 # 입력 받은 year, month중 트레이너의 근무 스케쥴이 꽉 차지 않은 날짜 배열을 리턴한다.
 @ns_schedule.route('/trainer/<int:trainer_id>/<int:year>/<int:month>')
 class TrainerMonthSchedule(Resource):
+    @ns_schedule.doc(description='트레이너의 스케쥴 조정이 가능한 날을 리턴한다.',
+                     params={
+                         'trainer_id': '트레이너 id',
+                         'year': '년',
+                         'month': '월'
+                     })
     def get(self, trainer_id, year, month):
         # 1단계: 트레이너의 전체 가능 요일 조회
         available_week_days = db.session.query(
@@ -175,6 +208,13 @@ class TrainerMonthSchedule(Resource):
 
 @ns_schedule.route('/trainer/<int:trainer_id>/<int:year>/<int:month>/<int:day>')
 class TrainerDaySchedule(Resource):
+    @ns_schedule.doc(description='트레이너의 하루 스케쥴 리스트를 리턴한다.',
+                     params={
+                         'trainer_id': '트레이너 id',
+                         'year': '년',
+                         'month': '월',
+                         'day': '일'
+                     })
     def get(self, trainer_id, year, month, day):
         date_filter = date(year, month, day)
 
@@ -224,7 +264,7 @@ class TrainerDaySchedule(Resource):
             'schedule_start_time': schedule.schedule_start_time.strftime(DATETIMEFORMAT)
         } for schedule in schedules]
 
-        return jsonify(result)
+        return result
 
 
 # todo: 스케쥴 조회시 스케쥴 상태 조건 추가
@@ -232,6 +272,13 @@ class TrainerDaySchedule(Resource):
 
 @ns_schedule.route('/trainer/<int:trainer_id>/<int:year>/<int:month>/<int:day>/week')
 class TrainerWeekSchedule(Resource):
+    @ns_schedule.doc(description='트레이너의 일주일 스케쥴 리스트를 리턴한다.',
+                     params={
+                         'trainer_id': '트레이너 id',
+                         'year': '년',
+                         'month': '월',
+                         'day': '일'
+                     })
     def get(self, trainer_id, year, month, day):
         start_date = date(year, month, day)
         end_date = date(year, month, day + 7)
