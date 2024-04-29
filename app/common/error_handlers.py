1ec846d1d9a454a63c7efc7540510706c77c6bed
TaeYeong Im <imtaebari@gmail.com>
2024-04-29 21:16:17 +0900

    Request Body 파싱할때 발생하는 에러 처리

--- app/common/error_handlers.py
+++ app/common/error_handlers.py
@@ -1,3 +1,5 @@
+import http.client
+
 from app.common.exceptions import ApplicationError
 
 
@@ -6,3 +8,10 @@ def register_error_handlers(app):
     def handle_application_error(error):
         return {"message": error.message}, error.status_code
 
+    @app.errorhandler(ValueError)
+    def handle_value_error(error):
+        return {"message": str(error)}, http.HTTPStatus.BAD_REQUEST
+
+    @app.errorhandler(KeyError)
+    def handle_key_error(error):
+        return {"message": f"Request Parameter None:{str(error)}"}, http.HTTPStatus.BAD_REQUEST
