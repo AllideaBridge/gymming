83b26ebf3203816330a7215cd95c56f2b3eb392e
dogyunkim <61974613+dogyun-k@users.noreply.github.com>
2024-06-03 19:20:23 +0900

    Feat updating change-ticket api (#23)

--- app/repositories/repository_change_ticket.py
+++ app/repositories/repository_change_ticket.py
@@ -6,6 +6,16 @@ class ChangeTicketRepository:
     def __int__(self):
         pass
 
+    def select_change_ticket_by_id(self, change_ticket_id) -> ChangeTicket:
+        return db.session.query(ChangeTicket).filter_by(id=change_ticket_id).first()
+
     def insert_change_ticket(self, data: ChangeTicket):
         db.session.add(data)
+        db.session.commit()
+
+    def update_change_ticket(self, change_ticket: ChangeTicket):
+        db.session.commit()
+
+    def delete_change_ticket(self, change_ticket: ChangeTicket):
+        db.session.delete(change_ticket)
         db.session.commit()
