ac6686bf9b66033f50d6b7deb351e2e3fc4b7fb0
dogyun <kdk124578@naver.com>
2024-07-01 20:18:25 +0900

    Code format

--- app/routes/route_change_ticket.py
+++ app/routes/route_change_ticket.py
@@ -2,7 +2,7 @@ from flask import jsonify, request
 from flask_restx import Namespace, Resource
 from marshmallow import ValidationError
 
-from app.common.exceptions import ApplicationError, BadRequestError
+from app.common.exceptions import ApplicationError
 from app.routes.models.model_change_ticket import CreateChangeTicketRequest, UpdateChangeTicketRequest
 from app.services.service_change_ticket import ChangeTicketService
 
@@ -111,8 +111,6 @@ class UserChangeTicketHistory(Resource):
         page = request.args.get('page')
         result = self.change_ticket_service.get_user_change_ticket_history(user_id, page)
         return result
-
-
 
 # 컨트롤러의 역할 : 유효성 검사
 # todo : 서비스 레이어로 비즈니스 로직 빼기.
